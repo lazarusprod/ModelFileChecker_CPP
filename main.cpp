f37f92c1e913e254cfc93a8d8632451e7caf2ef1
Revan <file@notfound.com>
2021-03-21 13:07:03 +0100

    Added Copyright

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include "Include/ReadModelFile.h";
 
 int main() {
-
-    /**
-     * Copyright (c) 2021 Lazarus Productions.
-    */
+    
     ReadModelFile rmf;
     rmf.readTheFile("/users/rene/Desktop/Solo_12.cfg");
 
